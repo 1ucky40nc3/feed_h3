6a9a5be2cbb21e410c1229fbf895e4ad786a1713
1ucky40nc3 <louisnwendler@gmail.com>
2023-02-17 17:08:59 +0100

    added new tests

---
+++ tests/feed_h3/models/ssm_seq/test_configuration_ssm_seq.py
@@ -0,0 +1,17 @@
+from feed_h3 import (
+    SSMConfig,
+    AttnConfig,
+    SSMSeqConfig
+)
+
+
+def test_ssm_config():
+    config = SSMConfig()
+
+
+def test_attn_config():
+    config = AttnConfig()
+
+
+def test_ssm_seq_config():
+    config = SSMSeqConfig()
