7e64e156f607d0abd5755f35de2a1c70b7663ba0
1ucky40nc3 <louisnwendler@gmail.com>
2023-03-13 19:54:50 +0100

    add pop method to configclass

--- src/feed_h3/utils/generic.py
+++ src/feed_h3/utils/generic.py
@@ -10,6 +10,12 @@ def configclass(cls=None, **kwargs):
         def __getitem__(self, key):
             return getattr(self, key)
         
+        def pop(self, key, default=None):
+            if key not in self.__dict__:
+                return default
+            return self.__dict__.pop()
+
+        setattr(cls, 'pop', pop)
         setattr(cls, 'keys', keys)
         setattr(cls, '__getitem__', __getitem__)
 
